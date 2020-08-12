0b2ac33afa3bd94b053ed0a7919e3c4571956514
g-freire <gustavomfreire@gmail.com>
2020-08-12 00:34:19 -0300

    starting CLI script

--- cli.py
+++ cli.py
@@ -3,33 +3,66 @@
 import requests
 from lxml import html
 from time import time,sleep
+import sys
+from io import StringIO
+import numpy as np
+import pandas as pd
+import requests
+import datetime
 
-"""
-REF: https://github.com/echiesse/desafios_programacao/blob/master/004-Cotacao_moedas.md
 
-https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/swagger-ui3#/
-https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='08-10-2020'&$top=100&$format=json
-https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaDia(moeda=@moeda,dataCotacao=@dataCotacao)?%40moeda='JPY'&%40dataCotacao='08-10-2020'&%24format=json" -H "accept: application/json;odata.metadata=minimal"
+def get_currecy_rank(date=None):
+    """
+    Routine to download from brazil central bank site two csv files
+    with currency information, preprocess then and generate a dataframe
+    containing a rank of the worst currency relative to USD current value
 
-https://www3.bcb.gov.br/bc_moeda/rest/cotacao/fechamento/ultima/1/005/2020-08-05
+    :param date: iso format timestamp of a market date. eg: 20200811
+    :return: string with currency
+    """
 
-https://www.bcb.gov.br/estabilidadefinanceira/cotacoestodas
+    try:
 
-ENDPOINT DOWNLOAD
-https://www4.bcb.gov.br/Download/fechamento/20200810.csv
+        # FIRST DATAFRAME - ALL CURRENCIES VALUES AT MARKET CLOSE
 
-NOME PAISES
-https://www.bcb.gov.br/estabilidadefinanceira/todasmoedas
-https://www4.bcb.gov.br/Download/fechamento/M20200810.csv
+        currencies_prices_url = f"https://www4.bcb.gov.br/Download/fechamento/{date}.csv"
 
-"""
+        data_from_url = StringIO(requests.get(currencies_prices_url).text)
+        cols = ['Timestamp', 'Cod Moeda', 'Tipo', 'Moeda',
+                'Taxa Compra', 'Taxa Venda', 'Paridade Compra',
+                'Paridade Venda']
+        df = pd.read_csv(data_from_url, sep=";", names=cols)
 
-def get_currecy():
-    return "ok"
+        # PREPROCESSING CURRENCY VALUES
+        aux = df.set_index('Moeda')
+        usd = aux.loc['USD']
+        usd_value = float(usd['Taxa Compra'].replace(',', '.'))
+
+        # Casting strings to float
+        df["Taxa Compra"] = df["Taxa Compra"].str.replace(',', '.')
+        df["Paridade Compra"] = df["Paridade Compra"].str.replace(',', '.')
+
+        # new column with USD to Currency formula
+        new_df = df.copy(deep=True)
+        new_df['USD to Currency'] = usd_value / (new_df['Taxa Compra'].astype(float))
+
+        # sorting by the formula result
+        sorted_df = new_df.sort_values(by='USD to Currency', ascending=False)
+        a = sorted_df.head(5)
+        a   
+
+
+        currencies_info_url = f"https://www4.bcb.gov.br/Download/fechamento/M{date}.csv"
+
+        return date
+
+    except Exception as e:
+        print(str(e))
+
 
 
 
 if __name__ == "__main__":
     start = time()
-    get_currecy()
+    get_currecy_rank(20200811)
     print(f"Process took: {time() - start} seconds")
